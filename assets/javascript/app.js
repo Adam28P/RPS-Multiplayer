9f38dda4b099fd12ea0eecfae48a9081e0c86d87
Adam P <adamparalovos@hotmail.com>
2018-08-17 00:50:38 -0400

    initialize firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBjZDHlKAtCCZkG7o1WTNLn7xDtw4yLnes",
+        authDomain: "rps-multiplayer-9b274.firebaseapp.com",
+        databaseURL: "https://rps-multiplayer-9b274.firebaseio.com",
+        projectId: "rps-multiplayer-9b274",
+        storageBucket: "",
+        messagingSenderId: "428674838353"
+    };
+    
+    firebase.initializeApp(config);
+
 });
